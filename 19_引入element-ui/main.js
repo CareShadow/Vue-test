5a2d3220f145c5320811e7539e88f3fa2c1407d7
lxl <1845161518@qq.com>
2022-03-06 16:34:55 +0800

    element-ui案例练习

--- 19_引入element-ui/main.js
+++ 19_引入element-ui/main.js
@@ -2,11 +2,15 @@
 import Vue from 'vue'
 //引入App
 import App from './App.vue'
-// 
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
+//引入VueRouter
+import VueRouter from 'vue-router'
+// 引入路由器
+//import router from './router/index.js'
+// 引入全部样式
+// import 'element-ui/lib/theme-chalk/index.css'
+import {Button} from 'element-ui';
 
-Vue.use(ElementUI)
+Vue.use(Button)
 
 Vue.config.productionTip = false
 
